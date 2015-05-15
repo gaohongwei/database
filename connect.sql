f27c095bb2d0ea10ebcd598eab62461186def42e
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2015-05-15 09:40:50 -0700

    Update connect.sql

--- connect.sql
+++ connect.sql
@@ -4,3 +4,11 @@ show grants for 'root'@'%';
 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD 'password' WITH GRANT OPTION ;
 flush previleges; 
+
+select @pwd :=password('root');
+select @pwd;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD @pwd WITH GRANT OPTION ;
+
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+flush previleges; 
