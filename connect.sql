fe699c4d4614451c0ffb35457713c8db34796c09
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-08-19 10:39:39 -0700

    Update connect.sql

--- connect.sql
+++ connect.sql
@@ -1,24 +1,44 @@
-use mysql
-select user,host,password from user;
-show grants for 'root'@'%';
+dbrootroot
 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD 'password' WITH GRANT OPTION ;
-flush previleges; 
+Connect/default login
+  mysql is using by default the UNIX auth_socket plugin.
+  mysql -uroot # not working
+  sudo mysql -uroot # working
+    USE mysql;
+    SELECT User, Host, plugin FROM mysql.user;
+  +------------------+-----------+-----------------------+
+  | User             | Host      | plugin                |
+  +------------------+-----------+-----------------------+
+  | root             | localhost | auth_socket           |
+  | mysql.session    | localhost | mysql_native_password |
+  | mysql.sys        | localhost | mysql_native_password |
+  | debian-sys-maint | localhost | mysql_native_password |
+  +------------------+-----------+-----------------------+
 
-select @pwd :=password('root');
-select @pwd;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD @pwd WITH GRANT OPTION ;
+Use password authentication
+    UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+    UPDATE user SET plugin='auth_socket' WHERE User='root';
+    FLUSH PRIVILEGES;
+    exit;
+  service mysql restart
 
+Grant
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+    FLUSH PRIVILEGES;
+    exit;
+  service mysql restart
 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'%' =  PASSWORD('root');
-flush previleges; 
+Create user
+  CREATE USER 'root'@'localhost' IDENTIFIED BY '';
+  select user,host,plugin from user;
+  show grants for 'root'@'localhost';
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
 
-
-Config
-/etc/mysql/mysql.conf.d/mysqld.cnf
-bind-adress= 172.16.16.169
-
-sudo service mysql restart
-
-telnet 127.0.0.1 3306 
+Reset password
+  SET PASSWORD FOR 'root'@'%' =  PASSWORD('root');
+  select @pwd :=password('root');
+  select @pwd;
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD 'password' WITH GRANT OPTION ;
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY PASSWORD @pwd WITH GRANT OPTION ;
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+  flush previleges;
