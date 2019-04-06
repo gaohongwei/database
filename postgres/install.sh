16e00eba37cd44e15fa0f726abd1dcdd0fd9a7f3
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-04-05 17:33:01 -0700

    Update install.sh

--- postgres/install.sh
+++ postgres/install.sh
@@ -11,3 +11,23 @@ Start PostgreSQL.
 
 start the database server using:
   /usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l logfile start
+
+login
+  sudo -u postgres psql
+
+Display
+  \?: show all psql commands.
+  \h sql-command: show syntax on SQL command.
+  \c dbname [username]: Connect to database, with an optional username (or \connect).
+
+  Display Commands: You can append + to show more details.
+  \l: List all database (or \list).
+  \d: Display all tables, indexes, views, and sequences.
+  \dt: Display all tables.
+  \di: Display all indexes.
+  \dv: Display all views.
+  \ds: Display all sequences.
+  \dT: Display all types.
+  \dS: Display all system tables.
+  \du: Display all users.
+  \x auto|on|off: Toggle|On|Off expanded output mode.
